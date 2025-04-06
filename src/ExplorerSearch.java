e07a92654604a52d72512261403880311c40880e
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-04-06 14:10:37 -0700

    Implemented findStart() method and added a test

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,37 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+
+        int validMoves = 0;
+
+        // Find start
+        int[] start = findStart(island);
+
+        // (if there is a start, validMoves += 1)
+
+        // recursively explore island, adding to validMoves
+
+        return validMoves;
     }
+
+ 
+    /* Helper method to find starting position
+     * Input: int[][] island
+     * Output: int[] containing {r, c} where r = row of start, c = column of '0'
+     * Throws IllegalArgumentException if there is no '0' in island
+     * TODO: handle edge case where there is more than one '0' on island?
+     */
+    public static int[] findStart(int[][] island) {
+        // Loop through island until '0' found, return int array of row, column coordinates
+        for (int r = 0; r < island.length; r++) {
+            for (int c = 0; c < island[r].length; c++) {
+                if (island[r][c] == 0) {
+                    return new int[]{r, c};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No starting location indicated.");
+    }
+
+    // Helper method to return valid moves
 }
--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -15,6 +15,28 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
+    // findStart tests
+
+    @Test 
+    public void testFindStart_oneValidStart() {
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,2,1,0,1},
+            {1,1,1,2,1,1},
+        };
+
+        int[] actual = ExplorerSearch.findStart(island);
+        int[] expected = {3, 4};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    // test throws exception for no start
+    // test multiple starts
+    // test first is start / last is start
+
     // Add more tests here!
     // Come up with varied cases
 }
